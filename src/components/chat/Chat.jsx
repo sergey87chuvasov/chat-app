4c0783bd11a8476553299ca17f9c9a080dfeab34
Sergey87 <serge.techno@gmail.com>
2024-04-16 01:02:46 +0300

    added React Chat Component Design - 1 part

--- src/components/chat/Chat.jsx
+++ src/components/chat/Chat.jsx
@@ -1,7 +1,26 @@
 import './chat.css';
 
 function Chat() {
-  return <div className='chat'>Chat</div>;
+  return (
+    <div className='chat'>
+      <div className='top'>
+        <div className='user'>
+          <img src='./avatar.png' alt='ava pic' />
+          <div className='texts'>
+            <span>Serge Ch</span>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </div>
+        </div>
+        <div className='icons'>
+          <img src='./phone.png' alt='icaon pic' />
+          <img src='./video.png' alt='icaon pic' />
+          <img src='./info.png' alt='icaon pic' />
+        </div>
+      </div>
+      <div className='center'></div>
+      <div className='bottom'></div>
+    </div>
+  );
 }
 
 export default Chat;
