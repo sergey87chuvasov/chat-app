2d9dc0a250daf07a2b500ef47fb40706a7a0ffbd
Sergey87 <serge.techno@gmail.com>
2024-04-15 23:37:14 +0300

    added React Search Bar Design

--- src/components/list/chatList/ChatList.jsx
+++ src/components/list/chatList/ChatList.jsx
@@ -1,7 +1,24 @@
+import { useState } from 'react';
 import './chatList.css';
 
 function ChatList() {
-  return <div className='chatList'>ChatList</div>;
+  const [addMode, setAddMode] = useState(false);
+  return (
+    <div className='chatList'>
+      <div className='search'>
+        <div className='searchBar'>
+          <img src='/search.png' alt='search pic' />
+          <input type='text' placeholder='Search' />
+        </div>
+        <img
+          className='add'
+          src={addMode ? './minus.png' : './plus.png'}
+          alt='plus btn'
+          onClick={() => setAddMode((prev) => !prev)}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default ChatList;
