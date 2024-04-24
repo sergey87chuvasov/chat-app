2a6631265ae2062e28ad33c94d8b7d4b2a143c3e
Sergey87 <serge.techno@gmail.com>
2024-04-24 23:57:29 +0300

    work with Auth with Zustand State Management Tool - 1 part

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,23 @@ import Chat from './components/chat/Chat';
 import Detail from './components/detail/Detail';
 import Login from './components/login/Login';
 import Notification from './components/notification/Notification';
+import { useEffect } from 'react';
+import { onAuthStateChanged } from 'firebase/auth';
+import { auth } from './lib/firebase';
 
 const App = () => {
   const user = false;
+
+  useEffect(() => {
+    const unSub = onAuthStateChanged(auth, (user) => {
+      // console.log(user);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, []);
+
   return (
     <div className='container'>
       {user ? (
